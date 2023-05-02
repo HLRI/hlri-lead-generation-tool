7039521b47415682d5db7c9bf1f1e70d5e92f7dd
ehsanerfani98 <ehsan.bavaghar1989@gmail.com>
2023-05-01 20:01:25 -0700

    add recording tools to panel

--- app/Http/Controllers/Admin/RecordTools/RecordToolsController.php
+++ app/Http/Controllers/Admin/RecordTools/RecordToolsController.php
@@ -34,6 +34,7 @@ class RecordToolsController extends Controller
         return view('admin.recording-tools.sessions', compact(['sites']));
     }
 
+    
     public function show(Request $request)
     {
         $info = Record::where('session', $request->session)->first();
