55ad5c193557967df8b3228d8b42a45ba7fc32e9
ehsanerfani98 <ehsan.bavaghar1989@gmail.com>
2023-06-29 23:22:47 +0330

    check response

--- routes/web.php
+++ routes/web.php
@@ -38,7 +38,7 @@ Route::get('/check-status', function () {
         ];
         $response = Http::withHeaders($head)->post($ch, $body);
 
-        dd($response);
+        dd($response->getStatus());
 
 
         // if($response == 201){
